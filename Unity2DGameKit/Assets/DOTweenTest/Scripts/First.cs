182ad7e575c035675931d2103a3061aa3c3981d6
QuanMao <2541673770@qq.com>
2019-12-09 23:04:18 +0800

    DOTween Test1

---
+++ Unity2DGameKit/Assets/DOTweenTest/Scripts/First.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class First : MonoBehaviour
+{
+    public RectTransform playButton;
+    public RectTransform settingsButton;
+    public RectTransform exitButton;
+
+    private void Start()
+    {
+        playButton.DOAnchorPosX(0, 1);
+        settingsButton.DOAnchorPosX(0, 1);
+        exitButton.DOAnchorPosY(-200, 1);
+    }
+}
